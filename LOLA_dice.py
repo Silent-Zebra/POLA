7cc12f3fdd2f864632f41294cc6e767616649083
Stephen Zhao <szstephenzhao88@gmail.com>
2022-03-24 15:52:29 -0400

    updated with og coin game. Runs now, but coins still spawn under agents

--- LOLA_dice.py
+++ LOLA_dice.py
@@ -198,6 +198,146 @@ def print_policy_and_value_info(th, vals):
 
         print_info_on_sample_obs(sample_obs, th, vals)
 
+"""
+OG COIN GAME.
+COIN CANNOT SPAWN UNDER EITHER AGENT.
+"""
+class OGCoinGameGPU:
+    """
+    Vectorized Coin Game environment.
+    Note: slightly deviates from the Gym API.
+    """
+    NUM_AGENTS = 2
+    NUM_ACTIONS = 4
+    MOVES = torch.stack([
+        torch.LongTensor([0, 1]),
+        torch.LongTensor([0, -1]),
+        torch.LongTensor([1, 0]),
+        torch.LongTensor([-1, 0]),
+    ], dim=0).to(device)
+
+    def __init__(self, max_steps, batch_size, grid_size=3):
+        self.max_steps = max_steps
+        self.grid_size = grid_size
+        self.batch_size = batch_size
+        # The 4 channels stand for 2 players and 2 coin positions
+        self.ob_space_shape = [4, grid_size, grid_size]
+        self.NUM_STATES = np.prod(self.ob_space_shape)
+        self.available_actions = 4
+        self.step_count = None
+
+    def reset(self):
+        self.step_count = 0
+        self.red_coin = torch.randint(2, size=(self.batch_size,)).to(device)
+
+        red_pos_flat = torch.randint(self.grid_size * self.grid_size, size=(self.batch_size,)).to(device)
+        self.red_pos = torch.stack((red_pos_flat // self.grid_size, red_pos_flat % self.grid_size), dim=-1)
+
+        blue_pos_flat = (torch.randint(self.grid_size * self.grid_size - 1, size=(self.batch_size,)).to(device) + red_pos_flat) % (self.grid_size * self.grid_size)
+        self.blue_pos = torch.stack((blue_pos_flat // self.grid_size, blue_pos_flat % self.grid_size), dim=-1)
+
+        coin_pos_flat = torch.randint(self.grid_size * self.grid_size - 2, size=(self.batch_size,)).to(device)
+        minpos = torch.min(red_pos_flat, blue_pos_flat)
+        maxpos = torch.max(red_pos_flat, blue_pos_flat)
+        coin_pos_flat[coin_pos_flat >= minpos] += 1
+        coin_pos_flat[coin_pos_flat >= maxpos] += 1
+        x = torch.logical_and(minpos == maxpos,
+                          torch.randn_like(minpos.float()) < 1.0 / (
+                                      self.grid_size * self.grid_size - 1))
+        coin_pos_flat[x.long()] = minpos+1 # THIS IS TO FIX THE OFFSET BUG
+        # coin_pos_flat[torch.logical_and(minpos==maxpos, torch.randn_like(minpos.float()) < 1.0 / (self.grid_size*self.grid_size - 1))] = minpos+1 # THIS IS TO FIX THE OFFSET BUG
+        coin_pos_flat = coin_pos_flat % (self.grid_size * self.grid_size)
+        self.coin_pos = torch.stack((coin_pos_flat // self.grid_size, coin_pos_flat % self.grid_size), dim=-1)
+
+        # state = self._generate_state()
+        # observations = [state, state]
+        state = self._generate_state()
+        state2 = state.clone()
+        # print(state2.shape)
+        state2[:, 0] = state[:, 1]
+        state2[:, 1] = state[:, 0]
+        state2[:, 2] = state[:, 3]
+        state2[:, 3] = state[:, 2]
+        observations = [state, state2]
+        # print(state)
+        # print(state2)
+        # 1/0
+        return observations
+
+    def _generate_coins(self):
+        mask = torch.logical_or(self._same_pos(self.coin_pos, self.blue_pos), self._same_pos(self.coin_pos, self.red_pos))
+        self.red_coin = torch.where(mask, 1 - self.red_coin, self.red_coin)
+
+        red_pos_flat = self.red_pos[mask,0] * self.grid_size + self.red_pos[mask, 1]
+        blue_pos_flat = self.blue_pos[mask, 0] * self.grid_size + self.blue_pos[mask, 1]
+        coin_pos_flat = torch.randint(self.grid_size * self.grid_size - 2, size=(self.batch_size,)).to(device)[mask]
+        minpos = torch.min(red_pos_flat, blue_pos_flat)
+        maxpos = torch.max(red_pos_flat, blue_pos_flat)
+        coin_pos_flat[coin_pos_flat >= minpos] += 1
+        coin_pos_flat[coin_pos_flat >= maxpos] += 1
+        coin_pos_flat = coin_pos_flat % (self.grid_size * self.grid_size)
+        self.coin_pos[mask] = torch.stack((coin_pos_flat // self.grid_size, coin_pos_flat % self.grid_size), dim=-1)
+
+    def _same_pos(self, x, y):
+        return torch.all(x == y, dim=-1)
+
+    def _generate_state(self):
+        red_pos_flat = self.red_pos[:,0] * self.grid_size + self.red_pos[:, 1]
+        blue_pos_flat = self.blue_pos[:, 0] * self.grid_size + self.blue_pos[:, 1]
+
+        # coin_pos_flat = self.coin_pos[:,0] * self.grid_size + self.coin_pos[:,1]
+        coin_pos_flatter = self.coin_pos[:,0] * self.grid_size + self.coin_pos[:,1] + self.grid_size * self.grid_size * self.red_coin + 2 * self.grid_size * self.grid_size
+
+        state = torch.zeros((self.batch_size, 4*self.grid_size*self.grid_size)).to(device)
+
+        state.scatter_(1, coin_pos_flatter[:,None], 1)
+        state = state.view((self.batch_size, 4, self.grid_size*self.grid_size))
+
+        state[:,0].scatter_(1, red_pos_flat[:,None], 1)
+        state[:,1].scatter_(1, blue_pos_flat[:,None], 1)
+
+        return state.view(self.batch_size, 4, self.grid_size, self.grid_size)
+
+    def step(self, actions):
+        ac0, ac1 = actions
+
+        self.step_count += 1
+
+        self.red_pos = (self.red_pos + self.MOVES[ac0]) % self.grid_size
+        self.blue_pos = (self.blue_pos + self.MOVES[ac1]) % self.grid_size
+
+        # Compute rewards
+        red_matches = self._same_pos(self.red_pos, self.coin_pos)
+        red_reward = torch.zeros_like(self.red_coin)
+        red_reward[red_matches] = 1
+
+        blue_matches = self._same_pos(self.blue_pos, self.coin_pos)
+        blue_reward = torch.zeros_like(self.red_coin)
+        blue_reward[blue_matches] = 1
+
+        red_reward[torch.logical_and(blue_matches, self.red_coin)] -= 2
+        blue_reward[torch.logical_and(red_matches, 1 - self.red_coin)] -= 2
+
+        total_rb_matches = torch.logical_and(blue_matches, self.red_coin).float().mean()
+        total_br_matches = torch.logical_and(red_matches, 1 - self.red_coin).float().mean()
+
+        total_rr_matches = red_matches.float().mean() - total_rb_matches
+        total_bb_matches = blue_matches.float().mean() - total_br_matches
+
+        self._generate_coins()
+        reward = [red_reward.float(), blue_reward.float()]
+        state = self._generate_state()
+        observations = [state, state]
+        if self.step_count >= self.max_steps:
+            done = torch.ones_like(self.red_coin)
+        else:
+            done = torch.zeros_like(self.red_coin)
+
+        return observations, reward, done, (
+        total_rr_matches, total_rb_matches,
+        total_br_matches, total_bb_matches)
+
+
 class CoinGameGPU:
     """
     Vectorized Coin Game environment.
@@ -764,6 +904,7 @@ if __name__ == "__main__":
     parser.add_argument("--diff_coin_cost", type=float, default=-2.0, help="changes problem setting (the cost to the opponent when you pick up a coin of their colour)")
     parser.add_argument("--same_coin_reward", type=float, default=1.0, help="changes problem setting (the reward for picking up coin of same colour)")
     parser.add_argument("--grid_size", type=int, default=3)
+    parser.add_argument("--og_coin_game", action="store_true", help="use the original coin game formulation")
 
 
     use_baseline = True
@@ -776,7 +917,11 @@ if __name__ == "__main__":
     input_size = args.grid_size ** 2 * 4
     action_size = 4
 
-    env = CoinGameGPU(max_steps=args.len_rollout, batch_size=args.batch_size, grid_size=args.grid_size)
+    if args.og_coin_game:
+        env = OGCoinGameGPU(max_steps=args.len_rollout, batch_size=args.batch_size, grid_size=args.grid_size)
+
+    else:
+        env = CoinGameGPU(max_steps=args.len_rollout, batch_size=args.batch_size, grid_size=args.grid_size)
 
     if args.load_path is None:
         agent1 = Agent(input_size, args.hidden_size, action_size)
