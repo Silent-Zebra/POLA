a559a24596dec52bfe4b30fc260dbcfc05b1e305
Stephen Zhao <szstephenzhao88@gmail.com>
2022-05-15 16:12:22 -0400

    Cleanup code

--- result_plots.py
+++ result_plots.py
@@ -1,5 +1,5 @@
 import torch
-from LOLA_dice import Agent
+from LOLA_dice import Agent # needed
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -29,52 +29,50 @@ def load_from_checkpoint_new(ckpt_path):
     vs_fixed_strat_scores = ckpt_dict["vs_fixed_scores"]
     return agent1, agent2, info, scores, vs_fixed_strat_scores
 
-# TODO LOAD UP AND COMPARE THE LOLA COIN RESULTS. Rerun if needed
-# TODO Plot OM results too?!?
-
 file_path = "."
 
-plot_coin = True
+plot_coin = False # True
 
 if plot_coin:
-
-    # ckpts_pola = ["checkpoint_551_2022-04-21_21-04.pt", "checkpoint_551_2022-04-21_20-48.pt",
-    #          "checkpoint_301_2022-04-26_18-06.pt", "checkpoint_301_2022-04-26_18-18.pt",
-    #          "checkpoint_301_2022-04-26_18-22.pt"]
 
     ckpts_pola = ["checkpoint_200_2022-05-08_18-39_seed10.pt",
                   "checkpoint_200_2022-05-08_15-15_seed11.pt",
                   "checkpoint_200_2022-05-08_15-24_seed12.pt",
                   "checkpoint_200_2022-05-09_03-41_seed13.pt",
-                  "checkpoint_200_2022-05-09_03-32_seed14.pt"]
-
-    # ckpts_lola = ["checkpoint_20000_2022-04-27_07-35.pt", "checkpoint_20000_2022-04-27_08-28.pt",
-    #               "checkpoint_20000_2022-04-27_07-56.pt", "checkpoint_20000_2022-04-27_07-41.pt"]
+                  "checkpoint_200_2022-05-09_03-32_seed14.pt",
+                  "checkpoint_200_2022-05-11_06-43_seed7.pt",
+                  "checkpoint_200_2022-05-11_00-39_seed8.pt",
+                  "checkpoint_200_2022-05-11_07-56_seed9.pt",
+                  "checkpoint_200_2022-05-12_02-42_seed5.pt",
+                  "checkpoint_200_2022-05-12_02-42_seed6.pt"]
 
     ckpts_lola = [
-                  # ["checkpoint_5000_2022-05-08_23-27_seed10.pt", "checkpoint_2000_2022-05-09_17-07_seed10.pt"],
+                  ["checkpoint_5000_2022-05-08_23-27_seed10.pt", "checkpoint_2000_2022-05-09_17-07_seed10.pt", "checkpoint_5000_2022-05-10_20-15_seed10.pt"],
                   ["checkpoint_5000_2022-05-08_23-26_seed11.pt", "checkpoint_2000_2022-05-09_17-03_seed11.pt", "checkpoint_8000_2022-05-10_09-55_seed11.pt"],
-                  # ["checkpoint_5000_2022-05-08_22-56_seed12.pt", "checkpoint_2000_2022-05-09_17-03_seed12.pt"],
+                  ["checkpoint_5000_2022-05-08_22-56_seed12.pt", "checkpoint_2000_2022-05-09_17-03_seed12.pt", "checkpoint_5000_2022-05-10_20-16_seed12.pt"],
                   "checkpoint_12000_2022-05-10_08-50_seed3.pt",
                   "checkpoint_12000_2022-05-10_14-25_seed1.pt",
-                  "checkpoint_12000_2022-05-10_14-42_seed2.pt"
+                  "checkpoint_12000_2022-05-10_14-42_seed2.pt",
+                  "checkpoint_14000_2022-05-11_05-03_seed4.pt",
+                  "checkpoint_14000_2022-05-11_05-06_seed5.pt",
+                  "checkpoint_14000_2022-05-11_05-20_seed6.pt",
+                  "checkpoint_14000_2022-05-11_05-29_seed7.pt" # There is kind of an arbitrary choice of seeds based on runs that got interrupted and based on which seeds I remembered having done vs not having done
                   ]
 
-    #
-    # ckpts_pola_om = ["checkpoint_201_2022-04-26_16-34.pt", "checkpoint_201_2022-04-26_16-37.pt",
-    #                  "checkpoint_200_2022-04-28_03-55.pt", "checkpoint_200_2022-04-28_04-24.pt",
-    #                  "checkpoint_200_2022-04-28_05-06.pt"]
+
     ckpts_pola_om = ["checkpoint_175_2022-05-10_14-38_seed6.pt",
                      "checkpoint_175_2022-05-10_10-56_seed7.pt",
-                     "checkpoint_175_2022-05-10_09-51_seed8.pt",]
+                     "checkpoint_175_2022-05-10_09-51_seed8.pt",
+                     "checkpoint_200_2022-05-11_11-08_seed1.pt",
+                     "checkpoint_200_2022-05-11_10-48_seed2.pt",
+                     "checkpoint_200_2022-05-11_11-06_seed3.pt",
+                     "checkpoint_200_2022-05-11_11-00_seed4.pt",
+                     "checkpoint_175_2022-05-11_12-28_seed5.pt",
+                     "checkpoint_200_2022-05-12_12-51_seed9.pt",
+                     "checkpoint_200_2022-05-12_13-04_seed10.pt"]
 
 else:
-    # ckpts_pola = ["checkpoint_200_2022-05-03_23-07.pt", "checkpoint_200_2022-05-03_22-49.pt",
-    #               "checkpoint_200_2022-05-03_22-32.pt"]
-    # ckpts_lola = ["checkpoint_10000_2022-05-04_00-00.pt", "checkpoint_10000_2022-05-04_00-03.pt",
-    #               "checkpoint_10000_2022-05-03_23-42.pt"]
-    # ckpts_pola_om = ["checkpoint_150_2022-05-03_23-17.pt", "checkpoint_150_2022-05-03_22-37.pt",
-    #                  "checkpoint_150_2022-05-03_23-34.pt"]
+    # PLOT IPD
     ckpts_pola = ["checkpoint_250_2022-05-05_12-13.pt", # seed 3
                   "checkpoint_250_2022-05-05_12-16.pt", # seed 4
                   "checkpoint_250_2022-05-05_12-19.pt", # seed 5
@@ -82,16 +80,21 @@ else:
                   ["checkpoint_100_2022-05-06_17-51.pt", "checkpoint_50_2022-05-07_03-48.pt"], # seed 2
                   ["checkpoint_100_2022-05-06_17-49.pt", "checkpoint_50_2022-05-07_03-48_seed6.pt"], # seed 6
                   ["checkpoint_100_2022-05-06_17-57.pt", "checkpoint_50_2022-05-07_03-40.pt"],  # seed 7
-                  # ["checkpoint_50_2022-05-06_15-44.pt", "checkpoint_50_2022-05-07_03-42.pt"],  # seed 8
-                  # ["checkpoint_50_2022-05-06_15-45.pt", "checkpoint_50_2022-05-07_04-23.pt"],  # seed 9
-                  # ["checkpoint_50_2022-05-06_15-44.pt", "checkpoint_50_2022-05-07_03-42.pt"],  # seed 10
-                  # "checkpoint_350_2022-05-05_12-04.pt",
-                  # "checkpoint_350_2022-05-05_04-08.pt",
-                  # "checkpoint_350_2022-05-05_04-05.pt"
+                  "checkpoint_200_2022-05-12_23-55_seed13.pt", # runs that didn't get interrupted
+                  "checkpoint_200_2022-05-13_00-22_seed14.pt",
+                  "checkpoint_200_2022-05-13_00-28_seed15.pt",
                   ]
     ckpts_lola = ["checkpoint_10000_2022-05-04_00-00.pt",
                   "checkpoint_10000_2022-05-04_00-03.pt",
-                  "checkpoint_10000_2022-05-03_23-42.pt"]
+                  "checkpoint_10000_2022-05-03_23-42.pt",
+                  "checkpoint_15000_2022-05-12_09-44_seed10.pt",
+                  "checkpoint_15000_2022-05-12_04-21_seed11.pt",
+                  "checkpoint_15000_2022-05-12_04-22_seed12.pt",
+                  "checkpoint_15000_2022-05-12_04-33_seed13.pt",
+                  "checkpoint_15000_2022-05-15_00-46_seed14.pt",
+                  "checkpoint_15000_2022-05-14_18-24_seed15.pt",
+                  "checkpoint_15000_2022-05-14_16-20_seed16.pt"
+                  ]
     ckpts_pola_om = ["checkpoint_150_2022-05-05_10-03.pt", # seed 3
                      "checkpoint_150_2022-05-05_10-05.pt", # seed 4
                      "checkpoint_150_2022-05-05_10-07.pt", # seed 5
@@ -99,14 +102,22 @@ else:
                      ["checkpoint_50_2022-05-06_16-34.pt", "checkpoint_50_2022-05-06_23-40.pt", "checkpoint_50_2022-05-07_04-32.pt"],  # seed 2
                      ["checkpoint_50_2022-05-06_16-35_seed6.pt", "checkpoint_50_2022-05-07_04-27.pt"],  # seed 6
                      ["checkpoint_50_2022-05-06_16-35_seed7.pt", "checkpoint_50_2022-05-07_04-23.pt"],  # seed 7
-                     # ["", ""],  # seed 8
-                     # ["", ""],  # seed 9
+                     ["checkpoint_200_2022-05-13_03-48_seed8.pt"],
+                     "checkpoint_200_2022-05-13_07-19_seed9.pt",
+                     "checkpoint_200_2022-05-13_09-15_seed10.pt",
                      ]
-    ckpts_static = ["checkpoint_150_2022-05-05_18-13.pt",
-                    "checkpoint_150_2022-05-05_18-30.pt",
-                    "checkpoint_150_2022-05-05_18-47.pt",
-                    "checkpoint_150_2022-05-05_19-04.pt",
-                    "checkpoint_150_2022-05-05_19-21.pt"]
+    ckpts_static = [
+                    "checkpoint_2_2022-05-15_17-47_seed1.pt",
+                    "checkpoint_2_2022-05-15_17-47_seed2.pt",
+                    "checkpoint_2_2022-05-15_17-48_seed3.pt",
+                    "checkpoint_2_2022-05-15_17-48_seed4.pt",
+                    "checkpoint_2_2022-05-15_17-48_seed5.pt",
+                    "checkpoint_2_2022-05-15_17-44_seed6.pt",
+                    "checkpoint_2_2022-05-15_17-45_seed7.pt",
+                    "checkpoint_2_2022-05-15_17-45_seed8.pt",
+                    "checkpoint_2_2022-05-15_17-46_seed9.pt",
+                    "checkpoint_2_2022-05-15_17-46_seed10.pt",
+                    ]
 
 
 def get_prop_same_coins(ckpts, max_iter_plot=200):
@@ -126,7 +137,6 @@ def get_score_individual_ckpt(full_path, w_coin_record=False):
     agent1, agent2, info, scores, vs_fixed_strat_scores = load_from_checkpoint_new(
         full_path)
 
-    # vs_fixed_strat_scores = torch.cat(vs_fixed_strat_scores)
     agent1_vs_fixed_strat_scores, agent2_vs_fixed_strat_scores = vs_fixed_strat_scores
     agent1_vs_fixed_strat_scores = torch.stack(agent1_vs_fixed_strat_scores)
     agent2_vs_fixed_strat_scores = torch.stack(agent2_vs_fixed_strat_scores)
@@ -210,19 +220,6 @@ def plot_coins_record(ckpts, max_iter_plot, label, z_score=1.96, skip_step=0):
     plot_with_conf_bounds(prop_same_coins_record, max_iter_plot, len(ckpts), label, skip_step,
                           z_score)
 
-    # avg_same_coins_total = prop_same_coins_record.mean(dim=0)
-    #
-    # stdev = torch.std(prop_same_coins_record, dim=0)
-    #
-    # upper_conf_bound = avg_same_coins_total + z_score * stdev / np.sqrt(len(ckpts))
-    # lower_conf_bound = avg_same_coins_total - z_score * stdev / np.sqrt(len(ckpts))
-    #
-    # # print(avg_same_coins_total)
-    # plt.plot(np.arange(max_iter_plot) * skip_step, avg_same_coins_total, label=label)
-    # plt.fill_between(np.arange(max_iter_plot) * skip_step, lower_conf_bound, upper_conf_bound, alpha=0.3)
-    # # plt.fill_between(np.arange(max_iter_plot), lower_conf_bound, upper_conf_bound, edgecolor='b', facecolor='b', alpha=0.3)
-
-
 def plot_with_conf_bounds(record, max_iter_plot, num_ckpts, label, skip_step, z_score, use_ax=False, ax=None):
     avg = record.mean(dim=0)
 
@@ -235,26 +232,21 @@ def plot_with_conf_bounds(record, max_iter_plot, num_ckpts, label, skip_step, z_
 
     if use_ax:
         assert ax is not None
-
         ax.plot(np.arange(max_iter_plot) * skip_step, avg,
              label=label)
-
         ax.fill_between(np.arange(max_iter_plot) * skip_step, lower_conf_bound,
                      upper_conf_bound, alpha=0.3)
 
     else:
-
-        # print(avg_same_coins_total)
         plt.plot(np.arange(max_iter_plot) * skip_step, avg,
                  label=label)
         plt.fill_between(np.arange(max_iter_plot) * skip_step, lower_conf_bound,
                          upper_conf_bound, alpha=0.3)
 
 
-
 def setup_ipd_plots(titles):
     nfigs = len(titles)
-    fig, axs = plt.subplots(1, nfigs, figsize=(5 * (nfigs) + 3, 5))
+    fig, axs = plt.subplots(1, nfigs, figsize=(5 * (nfigs) + 3, 4))
 
     for i in range(nfigs):
         axs[i].set_title(titles[i])
@@ -290,17 +282,6 @@ def plot_ipd_results(axs, ckpts, nfigs, max_iter_plot, label, z_score=1.96, skip
                               skip_step, z_score, use_ax=True, ax=axs[i])
         axs[i].legend()
 
-    # avg_same_coins_total = prop_same_coins_record.mean(dim=0)
-    #
-    # stdev = torch.std(prop_same_coins_record, dim=0)
-    #
-    # upper_conf_bound = avg_same_coins_total + z_score * stdev / np.sqrt(len(ckpts))
-    # lower_conf_bound = avg_same_coins_total - z_score * stdev / np.sqrt(len(ckpts))
-    #
-    # plt.plot(np.arange(max_iter_plot) * skip_step, avg_same_coins_total, label=label)
-    # plt.fill_between(np.arange(max_iter_plot) * skip_step, lower_conf_bound, upper_conf_bound, alpha=0.3)
-
-
 def plot_coin_results(axs, ckpts, nfigs, max_iter_plot, label, z_score=1.96, skip_step=0):
 
     score_record, avg_vs_alld_record, avg_vs_allc_record, avg_vs_tft_record, prop_same_coins_record = \
@@ -316,7 +297,7 @@ def plot_coin_results(axs, ckpts, nfigs, max_iter_plot, label, z_score=1.96, ski
 
 
 if plot_coin:
-    pola_max_iters = 150 #175 #125 #135 #105
+    pola_max_iters = 150
     pola_skip_step = 200
     lola_skip_step = 3
     lola_max_iters = pola_max_iters * pola_skip_step // lola_skip_step
@@ -324,16 +305,13 @@ if plot_coin:
     titles = ("Proportion of Same Coins Picked Up", "Average Score vs Each Other", "Average Score vs Always Defect")
     fig, axs = setup_coin_plots(titles)
 
-    # plot_coins_record(ckpts_pola, max_iter_plot=200, skip_step=200, label="POLA-DiCE")
     # POLA is 200 skip step because 1 inner 1 outer, 100 times = 200 env rollouts per epoch per agent
     plot_coin_results(axs, ckpts_pola, nfigs=len(titles), max_iter_plot=pola_max_iters, skip_step=pola_skip_step, label="POLA-DiCE")
 
-    # plot_coins_record(ckpts_lola, max_iter_plot=20000, skip_step=2, label="LOLA-DiCE")
     # LOLA is 2 skip step because 1 inner 1 outer, 1 time = 2 env rollouts per epoch per agent
     plot_coin_results(axs, ckpts_lola, nfigs=len(titles), max_iter_plot=lola_max_iters, skip_step=lola_skip_step, label="LOLA-DiCE")
 
-    # plot_coins_record(ckpts_pola_om, max_iter_plot=200, skip_step=200, label="POLA-OM")
-    # For OM I guess I'm not counting the env rollouts used for the OM data collection...
+    # For OM I'm not counting the env rollouts used for the OM data collection
     plot_coin_results(axs, ckpts_pola_om, nfigs=len(titles), max_iter_plot=pola_max_iters, skip_step=pola_skip_step, label="POLA-OM")
 
 else:
@@ -346,11 +324,10 @@ else:
     titles = ("Average Score vs Each Other", "Average Score vs Always Defect")
     fig, axs = setup_ipd_plots(titles)
 
-    # plot_ipd_results(axs, ckpts_pola, nfigs=len(titles), max_iter_plot=150, skip_step=200, label="POLA-DiCE")
     plot_ipd_results(axs, ckpts_pola, nfigs=len(titles), max_iter_plot=pola_max_iters, skip_step=pola_skip_step, label="POLA-DiCE")
     plot_ipd_results(axs, ckpts_lola, nfigs=len(titles), max_iter_plot=lola_max_iters, skip_step=lola_skip_step, label="LOLA-DiCE")
     plot_ipd_results(axs, ckpts_pola_om, nfigs=len(titles), max_iter_plot=pola_max_iters, skip_step=pola_skip_step, label="POLA-OM")
-    plot_ipd_results(axs, ckpts_static, nfigs=len(titles), max_iter_plot=pola_max_iters, skip_step=pola_skip_step, label="Random (non-learning)")
+    plot_ipd_results(axs, ckpts_static, nfigs=len(titles), max_iter_plot=2, skip_step=pola_skip_step * 100, label="Random (non-learning)")
 
     x_vals = np.arange(pola_max_iters) * pola_skip_step
     axs[0].plot(x_vals, -1 * np.ones_like(x_vals), label="Always Cooperate")
